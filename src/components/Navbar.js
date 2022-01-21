f7e446ef645169ef26c42527d1c9895b824908d8
Victoria <victoria@Victorias-MacBook-Air.local>
2022-01-20 20:07:45 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return <div className="flex">
+      <button>Home</button>
+      <button>Contact</button>
+      <button>Index</button>
+  </div>;
+};
+
+export default Navbar;
